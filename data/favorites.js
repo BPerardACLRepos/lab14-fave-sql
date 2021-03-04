478d70bd75d9cf6e6ddb73ab7d82469b72d79b2f
Brandon Perard <brandon.perard@gmail.com>
2021-03-03 17:37:35 -0800

    update data and tables

---
+++ data/favorites.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    fact_api_id: "591f98703b90f7150a19c130",  //_id
+    fact: "It has been scientifically proven that owning cats is good for our health and can decrease the occurrence of high blood pressure and other illnesses.",  //text
+    pic_url: "https://cdn2.thecatapi.com/images/gd.jpg", //url  thecatapi
+    owner_id: 1,
+  },
+  {
+    fact_api_id: "5a4d767c6ef087002174c28a",
+    fact: "Many Egyptians worshipped the goddess Bast, who had a woman’s body and a cat’s head.",
+    pic_url: "https://cdn2.thecatapi.com/images/a3v.jpg",
+    owner_id: 1,
+  },
+  {
+    fact_api_id: "591f9894d369931519ce35df",
+    fact: "Cat litter was \"invented\" in 1947 when Edward Lowe asked his neighbor to try a dried, granulated clay used to sop up grease spills in factories. (In 1990, Mr. Lowe sold his business for $200 million.)",
+    pic_url: "https://cdn2.thecatapi.com/images/bq2.jpg",
+    owner_id: 1,
+  },
+  {
+    fact_api_id: "5d38afd60f1c57001592f127",
+    fact: "In the early 1980s, a Chinchilla Persian cat named Jemari Sanquist mated with a Lilac Burmese named Bambino Lilac Fabergé, producing the Burmilla breed.",
+    pic_url: "https://cdn2.thecatapi.com/images/d35.jpg",
+    owner_id: 1,
+  },
+];
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const favorites = require('./favorites.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -11,37 +11,35 @@ async function run() {
   try {
     await client.connect();
 
-    const users = await Promise.all(
+    await Promise.all(
       usersData.map(user => {
         return client.query(`
                       INSERT INTO users (email, hash)
                       VALUES ($1, $2)
                       RETURNING *;
                   `,
-        [user.email, user.hash]);
+          [user.email, user.hash]);
       })
     );
-      
-    const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      favorites.map(favorite => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
-                    VALUES ($1, $2, $3);
+                    INSERT INTO favorites (fact_api_id, fact, pic_url, owner_id)
+                    VALUES ($1, $2, $3, $4);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+          [favorite.fact_api_id, favorite.fact, favorite.pic_url, favorite.owner_id]);
       })
     );
-    
+
 
     console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
   }
-  catch(err) {
+  catch (err) {
     console.log(err);
   }
   finally {
     client.end();
   }
-    
+
 }
